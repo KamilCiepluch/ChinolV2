7d92424d0547d961545efcb3b82ff9bad25bb9e0
Michal Tracz 236000 <236000@edu.p.lodz.pl>
2022-06-01 12:27:43 +0200

    dodanie main do Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Game {
-	Board board=new Board();
+
 	List<Player> playerList=new ArrayList<>();
+	void initializePlayers()
+	{
+
+	}
+	public static void main(String[] args) {
+		Board board=new Board();
+		board.boardInitialize();
+		System.out.println("Podaj liczbe graczy: ");
+		Scanner scanner = new Scanner(System.in);
+		int wybor=scanner.nextInt();
+		if(wybor<2 || wybor>4)
+			System.out.println("Zły wybór");
+
+	}
 }
