77233e72939021dfd9153dba7fbf42d029b9d5b2
KamilCiepluch <235845@edu.p.lodz.pl>
2022-06-01 09:40:20 +0200

    inicjalizacja planszy

--- src/Board.java
+++ src/Board.java
@@ -7,4 +7,28 @@ public class Board {
 
 
 
+
+
+
+	private void boardInitialize()
+	{
+		int planasza_size = 48;
+		for(int i=0; i<planasza_size; i++)
+		{
+			board.add(new Field(i));
+		}
+
+		board.get(0).setStart("red");
+		board.get(12).setStart("yellow");
+		board.get(24).setStart( "blue");
+		board.get(36).setStart( "green");
+	}
+
+	public static void main(String[] args) {
+		Board board  = new Board();
+		board.boardInitialize();
+		int x;
+		System.out.println("Xd");
+	}
+
 }
--- src/Field.java
+++ src/Field.java
@@ -1,9 +1,28 @@
 public class Field {
-
 	int fieldID;
 	boolean startYellow;
 	boolean startGreen;
 	boolean startBlue;
 	boolean startRed;
 
+
+	void setStart(String color)
+	{
+		switch (color) {
+			case "yellow" -> startYellow = true;
+			case "red" -> startRed = true;
+			case "blue" -> startBlue = true;
+			case "green" -> startGreen = true;
+		}
+	}
+
+
+	Field(int id)
+	{
+		fieldID = id;
+		startGreen = false;
+		startBlue = false;
+		startYellow = false;
+		startRed  =false;
+	}
 }
